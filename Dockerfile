942282e6562c7c543b395de8141f07df0584d1be
Len Macarthy <lgmacart@hotmail.com>
2025-02-12 15:25:55 -0600

    Add route to access API.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
+RUN apt-get -y update
+RUN apt-get -y install curl
 
 COPY . .
 
--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
+import subprocess
+import json
 
 app = Flask(__name__)
+
+def getRandomNumber(uri):
+    command = "curl -s " + uri
+    #try:
+    result = subprocess.run(["curl", "-s", uri], check=True, capture_output=True).stdout
+    #except:
+    #    return "Junk"
+    jsonReturn = json.loads(result)
+    return str(jsonReturn["number"])
 
 @app.route('/')
 def index():
@@ -11,6 +22,10 @@ def hello():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     return 'Hello, %s %s! Have fun learning Python. <br/> <a href="/">Back Home</a>' % (first_name, last_name)
+
+@app.route('/randomnumber')
+def randomnumber():
+    return 'Your random number is: %s' % getRandomNumber("192.1.1.50:3000/randomnumber")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
